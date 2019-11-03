a3fd7e37aa5e8b501394f2e7cfccd397d7e5d36f
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-03 05:13:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,7 +17,16 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
-  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
   
 }
 
