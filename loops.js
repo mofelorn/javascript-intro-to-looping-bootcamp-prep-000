26ffa2fd5b1eb13cf42694004d344d78c8aa0661
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-03 05:44:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,5 +26,6 @@ function incrementVariable() {
 function doWhileLoop(num) {
   do {
     console.log("I run once regardless.")
-  } while (incrementVariable() < num) 
+  } 
+  while (incrementVariable() < num) 
 }
